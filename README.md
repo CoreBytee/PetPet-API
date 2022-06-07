58403635a3573296f30dff17026f1c6097555a1d
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2022-06-07 20:23:10 +0200

    done

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # PetPet-API
- Create Pet Pet images with a simple api
+Create Pet Pet images with a simple api\
+Use it like this: `https://pet.corebyte.ga/?url=[URL]`
---
+++ index.js
@@ -0,0 +1,27 @@
+const App = require('express')()
+const Pet = require('pet-pet-gif')
+
+App.get('/', async function(Request, Response) {
+    console.log('Request')
+    if (Request.query.url == null) {
+        Response.send('Please provide a url')
+        return
+    }
+    var GifData
+    try
+    {
+        GifData = await Pet(Request.query.url)
+        Response.set("Content-Type", "image/gif")
+        Response.status(200).send(GifData)
+        console.log('Response')
+    }catch
+    {
+        Response.send('Error')
+        console.log("Error")
+    }
+    console.log('Done')
+    
+    
+})
+
+App.listen(80)
